d73d4760b591907d595312bd879ed5288be0cdb8
zodicto <minhtan210703@gmail.com>
2025-10-30 05:07:52 +0700

    feat: invoice api

---
+++ AptCare.Service/Services/Interfaces/IInvoiceService.cs
@@ -0,0 +1,16 @@
+﻿using AptCare.Service.Dtos.InvoiceDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AptCare.Service.Services.Interfaces
+{
+    public interface IInvoiceService
+    {
+        Task<string> CreateInternalInvoiceAsync(InvoiceInternalCreateDto dto);
+        Task<string> CreateExternalInvoiceAsync(InvoiceExternalCreateDto dto);
+        Task<IEnumerable<InvoiceDto>> GetInvoicesAsync(int repairRequestId);
+    }
+}
